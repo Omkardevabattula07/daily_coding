a586455568d415735a7a2864b5e70a404aeb9223
omkar <devabattulaosst1909@gmail.com>
2024-07-24 22:01:10 +0530

    Added a folder that will access your camera

--- 1_access_camera/ac.js
+++ 1_access_camera/ac.js
@@ -1,12 +1,17 @@
 let video=document.getElementById("camera");
 let msg = document.getElementById("message");
+let btn =document.getElementById("btn")
  //Getting the user data we use navigator.mediaDevices.getUserMedia
-if(navigator.mediaDevices.getUserMedia){
-    navigator.mediaDevices.getUserMedia({video:true}).then((stream)=>{
-        video.srcObject = stream;
-        //if the code is success then it will print success
-        msg.innerHTML="success"
-    },() =>{
-        msg.innerHTML = "An error occured";
-    }
-)};
+ // Here i use an event listner so that i can use this the button by click
+btn.addEventListener('click',()=>{
+    if(navigator.mediaDevices.getUserMedia){
+        navigator.mediaDevices.getUserMedia({video:true}).then((stream)=>{
+            video.srcObject = stream;
+            //if the code is success then it will print success
+            msg.innerHTML="success"
+        },() =>{
+            msg.innerHTML = "An error occured";
+        }
+    )};
+    
+})
